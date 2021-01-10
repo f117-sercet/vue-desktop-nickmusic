45646f4b4cd79932314ad30367fe29b49604a5bb
f117-serect <15159885771@163.com>
2021-01-10 10:52:22 +0800

    更新页面

---
+++ vue-desktop-music/src/components/common/empty/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="empty flex-column">
+        <img src="../../../assets/images/empty.png" alt="">
+        <p>{{ emptyText }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+
+            };
+        },
+        props: {
+            emptyText: {
+                type: String,
+                default: '别找了，没了'
+            }
+        },
+        components: {},
+        computed: {},
+        watch: {},
+        methods: {
+
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+    }
+</script>
+<style lang='stylus' scoped>
+    .empty {
+        width: 100%;
+        img {
+            width: 300px;
+        }
+    }
+</style>
