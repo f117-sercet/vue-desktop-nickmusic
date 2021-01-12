52cc78b988c15c3465675dbf6a95f4d002d031ff
f117-serect <15159885771@163.com>
2021-01-12 18:25:07 +0800

    页面更新

---
+++ vue-desktop-music/src/components/common/loader/index.vue
@@ -0,0 +1,20 @@
+<template>
+   <div class=""></div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {}
+        },
+        comments:{},
+        computed:{},
+        watch:{},
+        methods:{},
+        created() {},
+        mounted() {}
+    }
+
+</script>
+
+<style lang="stylus" scoped></style>
